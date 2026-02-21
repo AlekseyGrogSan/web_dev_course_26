3286e510aaea48b76e80b1fbe00d6d76c7a29b5a
grogSan <alexkernel05@gmail.com>
2026-02-21 21:14:33 +0300

    Apdate skript

--- labs/lab2/build_calendar.rb
+++ labs/lab2/build_calendar.rb
@@ -87,25 +87,71 @@ class GenerateCalendar
 
     return all_mathes
   end
+
+  def time_per_cnt(cnt_games)
+    res = Hash.new
+    case cnt_games
+    when 1
+      res[0] = 1
+      res[1] = 0
+      res[2] = 0
+    when 2
+      res[0] = 1
+      res[1] = 1
+      res[2] = 0
+    when 3
+      res[0] = 1
+      res[1] = 1
+      res[2] = 1
+    when 4
+      res[0] = 2
+      res[1] = 1
+      res[2] = 1
+    when 5
+      res[0] = 2
+      res[1] = 2
+      res[2] = 1
+    when 6
+      res[0] = 2
+      res[1] = 2
+      res[2] = 2
+    else
+      res[0] = 0
+      res[1] = 0
+      res[2] = 0
+    end
+    res
+  end
   def generate_matches
     calendar = []
     games_inds = []
+    good_days_ = good_days
     all_games = allMetches
     times_for_games = ["12:00", "15:00", "18:00"]
     flag_break = false
     cnt_propusk = 1
 
+    day_for_propusk = all_games.length.to_f /  good_days_.length
+    abs_dfp = day_for_propusk.ceil
+
+    raise ArgumentError, "Невозможно поместить такое кол-во матчей в данный промежуток" if abs_dfp > 6
+
+    cnt_games_per_time = time_per_cnt(abs_dfp)
+
+    sum_games_on_day = cnt_games_per_time.values.sum
+
+    razn = good_days_.length - all_games.length / sum_games_on_day
+    propusk = (good_days_.length.to_f / razn).ceil
+
     for day in good_days
-      # Пропускаем каждый 27-й день
-      if cnt_propusk % 27 == 0
+      # Пропускаем каждый n-ый день для равномерного расписания
+      if (cnt_propusk % propusk == 0)
         cnt_propusk += 1
         next
       end
-
       # Команды, которые уже играют в этот день
       weekend_teams = []
-
-      # Планируем до 3 игр в день
+      # Планируем до m игр в день
       (0).upto(2) do |i|
         # Проверяем, все ли игры использованы
         if games_inds.length >= all_games.length
@@ -113,35 +159,39 @@ class GenerateCalendar
           break
         end
 
-        # Ищем доступную игру
-        available_games = []
-        all_games.each_with_index do |game, index|
-          # Игра еще не использована И команды не заняты в этот день
-          if !games_inds.include?(index) && !weekend_teams.any? { |team| game.include?(team) }
-            available_games << index
+        break if cnt_games_per_time[i] == 0
+
+        1.upto(cnt_games_per_time[i]) do |j|
+          # Ищем доступную игру
+          available_games = []
+          all_games.each_with_index do |game, index|
+            # Игра еще не использована И команды не заняты в этот день
+            if !games_inds.include?(index) && !weekend_teams.any? { |team| game.include?(team) }
+              available_games << index
+            end
           end
-        end
 
-        if available_games.empty?
-          # Нет доступных игр для этого слота
-          next
-        end
-
-        # Выбираем случайную доступную игру
-        ind_game_1 = available_games.sample
-        games_inds.push(ind_game_1)
-
-        # Добавляем команды в список занятых на этот день
-        game = all_games[ind_game_1]
-        @teams.each do |team|
-          if game.include?(team)
-            weekend_teams.push(team)
+          if available_games.empty?
+            # Нет доступных игр для этого слота
+            next
           end
-        end
 
-        # Формируем запись об игре
-        game1 = "date: #{day}, time: #{times_for_games[i]}, #{game}"
-        calendar.push(game1)
+          # Выбираем случайную доступную игру
+          ind_game_1 = available_games.sample
+          games_inds.push(ind_game_1)
+
+          # Добавляем команды в список занятых на этот день
+          game = all_games[ind_game_1]
+          @teams.each do |team|
+            if game.include?(team)
+              weekend_teams.push(team)
+            end
+          end
+
+          # Формируем запись об игре
+          game1 = "date: #{day}, time: #{times_for_games[i]}, #{game}"
+          calendar.push(game1)
+        end
       end
 
       break if flag_break
@@ -152,8 +202,14 @@ class GenerateCalendar
 
   #Функция для записи календаря в файл
   def createCalendar
+    begin
+      calendar_games = generate_matches
+    rescue ArgumentError => e
+      puts(e.message)
+      return nil
+    end
     File.open(@outFile, "w") do |outFile|
-      for game in generate_matches
+      for game in calendar_games
         outFile.puts game + "\n"
       end
     end
